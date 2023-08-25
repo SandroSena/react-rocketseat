3626f5467279c57ee3af5676ff829c94759e45e2
sandro.sena <sandro340@hotmail.com>
2023-08-25 14:33:05 -0300

    Change structure

--- Desafio-01/src/App.tsx
+++ Desafio-01/src/App.tsx
@@ -3,15 +3,23 @@ import './global.css';
 
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
-import { Tasks } from './components/Tasks';
+import { TasksList } from './components/TasksList';
+
+const tasks = [
+  {
+    id: 1,
+    title: 'Completar o curso de React da Rocketseat',
+    hasCompleted: false,
+  },
+];
 
 function App() {
   return (
-    <>
+    <div className='mainAppWrapper'>
       <Header />
       <AddTask />
-      <Tasks />
-    </>
+      <TasksList />
+    </div>
   );
 }
 
