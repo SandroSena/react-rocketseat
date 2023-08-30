a878e0a07a42c10f0644e3b7abb1721c95dc1fa8
sandro.sena <sandro340@hotmail.com>
2023-08-30 10:53:39 -0300

    02 Estrutura da aplicação

---
+++ 02-ignite-timer-reactjs/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtoVariant, ButtonContainer } from './Button.styles'
+
+interface ButtonProps {
+  variant?: ButtoVariant
+}
+
+export const Button = ({ variant = 'primary' }: ButtonProps) => {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
