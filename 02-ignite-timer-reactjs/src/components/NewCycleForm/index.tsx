e20fce0577c5aa6450573a7b9271d81137f0129c
sandro.sena <sandro340@hotmail.com>
2023-10-20 10:38:23 -0300

    Finalizando a migração para contextApi

--- 02-ignite-timer-reactjs/src/components/NewCycleForm/index.tsx
+++ 02-ignite-timer-reactjs/src/components/NewCycleForm/index.tsx
@@ -1,60 +1,40 @@
-import { FormContainer, MinutesAmountInput, TaskInput } from "./style";
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as zod from 'zod';
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(1, 'O ciclo precisa ser de no minimo 5 minutos')
-    .max(60, 'O ciclo precisa ser de no maximo 60 minutos'),
-});
-
-interface NewCycleFormData {
-  task: string;
-  minutesAmount: number;
-}
-
-// type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+import { useContext } from 'react';
+import { CyclesContext } from '../../pages/Home';
+import { FormContainer, MinutesAmountInput, TaskInput } from './style';
+import { useFormContext } from 'react-hook-form';
 
 export const NewCycleForm = () => {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  });
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormContainer>
-          <label htmlFor='task'>Vou trabalhar em</label>
-          <TaskInput
-            id='task'
-            list='task-suggestions'
-            placeholder='Dê um nome para o seu projeto'
-            disabled={!!activeCycle}
-            {...register('task')}
-          />
+      <label htmlFor='task'>Vou trabalhar em</label>
+      <TaskInput
+        id='task'
+        list='task-suggestions'
+        placeholder='Dê um nome para o seu projeto'
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
 
-          <datalist id='task-suggestions'>
-            <option value='Minecraft Server List' />
-          </datalist>
+      <datalist id='task-suggestions'>
+        <option value='Minecraft Server List' />
+      </datalist>
 
-          <label htmlFor=''>durante</label>
-          <MinutesAmountInput
-            type='number'
-            id='minutesAmount'
-            placeholder='00'
-            disabled={!!activeCycle}
-            step={5}
-            min={1}
-            max={60}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
+      <label htmlFor=''>durante</label>
+      <MinutesAmountInput
+        type='number'
+        id='minutesAmount'
+        placeholder='00'
+        disabled={!!activeCycle}
+        step={5}
+        min={1}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
 
-          <span>minutos.</span>
-        </FormContainer>
-  )
-}
+      <span>minutos.</span>
+    </FormContainer>
+  );
+};
--- 02-ignite-timer-reactjs/src/pages/Home/index.tsx
+++ 02-ignite-timer-reactjs/src/pages/Home/index.tsx
@@ -5,9 +5,12 @@ import {
   StartCountdownButton,
   StopCountdownButton,
 } from './styles';
-import { createContext, useEffect, useState } from 'react';
-import { NewCycleForm } from '../../components/NewCycleForm';
+import { createContext, useState } from 'react';
 import { Countdown } from '../../components/Countdown';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+import { NewCycleForm } from '../../components/NewCycleForm';
 
 interface Cycle {
   id: string;
@@ -21,16 +24,48 @@ interface Cycle {
 interface CyclesContextType {
   activeCycle: Cycle | undefined;
   activeCycleId: string | null;
+  amountSecondsPassed: number;
   markCurrentCycleAsFinished: () => void;
+  setSecondsPassed: (seconds: number) => void;
 }
 
 export const CyclesContext = createContext({} as CyclesContextType);
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(1, 'O ciclo precisa ser de no minimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no maximo 60 minutos'),
+});
+
 export const Home = () => {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycledId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  interface NewCycleFormData {
+    task: string;
+    minutesAmount: number;
+  }
+
+  // type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  });
+
+  const { handleSubmit, watch, reset } = newCycleForm;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const setSecondsPassed = (seconds: number) => {
+    setAmountSecondsPassed(seconds);
+  };
 
   const markCurrentCycleAsFinished = () => {
     setCycles(
@@ -42,7 +77,7 @@ export const Home = () => {
         }
       })
     );
-  }
+  };
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
     const id = String(new Date().getTime());
@@ -81,8 +116,18 @@ export const Home = () => {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action=''>
-        <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}>
-          <NewCycleForm />
+        <CyclesContext.Provider
+          value={{
+            activeCycle,
+            activeCycleId,
+            markCurrentCycleAsFinished,
+            amountSecondsPassed,
+            setSecondsPassed
+          }}
+        >
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
           <Countdown />
         </CyclesContext.Provider>
         {activeCycle ? (
