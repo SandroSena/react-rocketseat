74051b89d32566ea8715baa59c15d5ce46b24cca
sandro.sena <sandro340@hotmail.com>
2024-03-14 12:38:09 -0300

    Modal de nova transação

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -2,25 +2,20 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/logo.svg';
+import NewTransactionModal from '../NewTransactionModal';
 
 const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt='logo' />
-        
+
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
