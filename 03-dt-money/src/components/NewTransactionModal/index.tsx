12384daa5822cac5a141b1fb2016282a92b385e4
sandro.sena <sandro340@hotmail.com>
2024-03-15 12:26:53 -0300

    Utilizando Radio Button para criar os botoes de entrada e saida

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 const NewTransactionModal = () => {
   return (
@@ -9,15 +15,26 @@ const NewTransactionModal = () => {
 
       <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
-        
+
         <CloseButton>
-          <X size={24}/>
-        </CloseButton> 
+          <X size={24} />
+        </CloseButton>
 
         <form action=''>
           <input type='text' placeholder='Descrição' required />
           <input type='number' placeholder='Preço' required />
           <input type='text' placeholder='Categoria' required />
+
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type='submit'>Cadastrar</button>
         </form>
